cd08e7bf400cf1f5efd74de162b147f9a74157f5
Caleb Caswell-Levy <calebcaswelllevy@gmail.com>
2020-12-27 18:04:44 -0700

    implemented isWon function

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 
-
+// To Do:
+    //Figure out how player moves are going to work
 let gameBoard = (function(){
     // initialize squares
-    let squares = []
+    let squares = [];
+    let choice = '';
+
     for (let i=0; i<9; i++){
         squares.push(" ");
     }
@@ -16,11 +19,7 @@ let gameBoard = (function(){
     function getSquare(square) {
         return squares[square]
     }
-    // add event listeners to squares
-    for (let i=0; i<9; i++) {
-        let ref = document.getElementById(`S${i}`)
-        ref.addEventListener('click', () => console.log(`square ${i} clicked`))
-    }
+   
 
     return {
         setSquare,
@@ -61,20 +60,66 @@ const game = () => {
     //create player objects
     let X = player('X');
     let O = player('O');
-
+    let choice = ""
     //Check for winner
     function isWon() {
-        //TO DO...
+        let won = false
+        let board = [];
+        for (let i=0; i<9; i++){
+            board.push(gameBoard.getSquare[i])
+        }
+        //Check rows
+        if (gameBoard.getSquare(0)===gameBoard.getSquare(1) === gameBoard.getSquare(2) !== " " ) {
+            return true;
+        } else if (gameBoard.getSquare(3)===gameBoard.getSquare(4) === gameBoard.getSquare(5) !== " " ) {
+            return true;
+        } else if (gameBoard.getSquare(6)===gameBoard.getSquare(7) === gameBoard.getSquare(8) !== " " ) {
+            return true;
+        }
+        //Check Columns
+        if (gameBoard.getSquare(0)===gameBoard.getSquare(3) === gameBoard.getSquare(6) !== " " ) {
+            return true;
+        } else if (gameBoard.getSquare(1)===gameBoard.getSquare(4) === gameBoard.getSquare(7) !== " " ) {
+            return true;
+        } else if (gameBoard.getSquare(2)===gameBoard.getSquare(5) === gameBoard.getSquare(8) !== " " ) {
+            return true;
+        }
+
+        // CHECK DIAGONALS
+        if (gameBoard.getSquare(0)===gameBoard.getSquare(4) === gameBoard.getSquare(8) !== " " ) {
+            return true;
+        } else if (gameBoard.getSquare(2)===gameBoard.getSquare(4) === gameBoard.getSquare(6) !== " " ) {
+            return true;
+        }
+        // CHECK FOR FULL BOARD
+        else if (!(board.includes(" "))) {
+            return true;
+        }
+        //else not over yet
+        return false;
     }
+
+     // add event listeners to squares
+     for (let i=0; i<9; i++) {
+        let ref = document.getElementById(`S${i}`)
+        ref.addEventListener('click', () => {
+            console.log(`square ${i} clicked`)
+            if (gameBoard.getSquare(i) != " ") {
+                return;
+            }
+            choice = i;
+        })
+    }
+
     // While loop that runs while 
     // won is false:
-    let won = true;
-    while (won === false) {
-        displayController.draw()
-        makeMove(O) 
-        }
+    let won = false;
+    //while (won === false) {
+      //  displayController.draw()
+        //O.move(choice) 
+        //}
 
 }
 
 displayController.draw()
-
+game()
